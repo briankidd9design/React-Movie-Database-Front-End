692c78ebef92c5cf4b91f13c2e203382774a8d92
Brian Kidd <brian.kidd.one@gmail.com>
2020-12-10 18:48:51 -0800

    deleted comments from pagination.jsx page so the paginaiton buttons render

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import _ from "lodash"; //underscore
+import _ from "lodash";
 import PropTypes from "prop-types";
 
 const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }) => {
-  // const { itemsCount, pageSize, onPageChange, currentPage} = props;
-  // console.log(currentPage);
-
   const pagesCount = Math.ceil(itemsCount / pageSize);
   const pages = _.range(1, pagesCount + 1);
   if (pagesCount === 1) return null;
@@ -14,7 +11,6 @@ const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }) => {
     <nav>
       <ul className="pagination">
         {pages.map((page) => (
-          //each child in an array or iterator should have a unique key
           <li
             key={page}
             className={page === currentPage ? "page-item active" : "page-item"}
@@ -28,7 +24,7 @@ const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }) => {
     </nav>
   );
 };
-//as you build components it is a good practice to use PropTypes to catch bugs related to type-setting.
+
 Pagination.propTypes = {
   itemsCount: PropTypes.number.isRequired,
   pageSize: PropTypes.number.isRequired,
